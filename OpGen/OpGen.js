46742102d1bbada2034855eef779dda329eeeaeb
Matt Gukowsky <Matt Gukowsky>
2016-07-29 11:50:19 -0400

    Add node.js OpGen project

---
+++ OpGen/OpGen.js
@@ -0,0 +1,10 @@
+﻿var _writer = require("./Writer.js");
+var Writer = _writer.Writer;
+
+(function main() {
+	var writer = new Writer("../moc/ops.h");
+	writer.writeln("baz");
+	writer.close();
+
+	process.exit(0);
+})();
